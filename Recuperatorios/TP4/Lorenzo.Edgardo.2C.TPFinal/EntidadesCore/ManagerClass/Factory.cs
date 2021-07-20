f8f03542fbc3730fc4a6fc73f2cee1b2b48eb446
toty88 <edgardolor@gmail.com>
2021-07-20 17:53:06 -0300

    se sube tp4

---
+++ Recuperatorios/TP4/Lorenzo.Edgardo.2C.TPFinal/EntidadesCore/ManagerClass/Factory.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntidadesCore
+{
+    public static class Factory
+    {
+        #region Atributes
+        public static List<Product> listaProductos;
+        private static readonly List<int> listOfProductsID;
+        private static readonly Random random;
+        public static readonly Materials stock;
+        #endregion
+
+        #region Constructor
+        static Factory()
+        {
+            listaProductos = new List<Product>();
+            listOfProductsID = new List<int>();
+            random = new Random(DateTime.Now.Millisecond);
+            stock = Materials.GetStock();
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Propiedad setter que Crea un producto y lo agrega a la lista, en caso de que haya stock 
+        /// y no tenga ID repetido
+        /// </summary>
+        public static Product Create
+        {
+            set
+            {
+                try
+                {
+                    value.Serial_Number = GenerateId(listOfProductsID, random);
+                    if (value.Serial_Number != -1
+                        && StockAvailable(value)
+                        && listaProductos + value
+                        && SubstractMaterials(value))
+                    {
+
+                    }
+                }
+                catch (OutOfStockException ex)
+                {
+                    throw new OutOfStockException(ex.Message, ex);
+                }
+                catch (SqlException ex)
+                {
+                    throw ex;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message, ex);
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Metodo Generic que resta stock del diccionario de materiales de la clase Materials
+        /// La clase Factory contiene un atributo de tipo Materials
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Un objeto que tambien tiene un atributo materiales de tipo diccionario</param>
+        /// <returns>true si se pudo restar materiales, false caso contrario </returns>
+        private static bool SubstractMaterials<T>(T obj) where T : Product
+        {
+            bool exit = false;
+            Dictionary<string, int> temp = new Dictionary<string, int>(stock.StockList);
+            foreach (string product_material in ((T)obj).MaterialsNeeded.Keys)
+            {
+                foreach (string stock_material in temp.Keys)
+                {
+                    if (stock_material == product_material)
+                    {
+                        stock.StockList[stock_material] -= ((T)obj).MaterialsNeeded[product_material];
+                        exit = true;
+                        break;
+                    }
+                }
+            }
+            return exit;
+        }
+        /// <summary>
+        /// Metodo Generic que chequea que haya stock disponible para poder crear el objeto
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Un objeto que tambien tiene un atributo materiales de tipo diccionario</param>
+        /// <returns></returns>
+        static bool StockAvailable<T>(T obj) where T : Product
+        {
+            bool hasEnoughMaterial = false;
+            foreach (string material_item in stock.StockList.Keys)
+            {
+                foreach (string product_item in ((T)obj).MaterialsNeeded.Keys)
+                {
+                    if (material_item == product_item)
+                    {
+                        if (stock.StockList[material_item] >= ((T)obj).MaterialsNeeded[product_item])
+                        {
+                            hasEnoughMaterial = true;
+                            break;
+                        }
+                        else
+                        {
+                            throw new OutOfStockException();
+                        }
+                    }
+                }
+            }
+            return hasEnoughMaterial;
+        }
+        /// <summary>
+        /// Metodo que genera un ID random y lo guarda en una lista de IDs (en caso de que no exista)
+        /// </summary>
+        /// <param name="list">La lista donde guardar el nuevo ID</param>
+        /// <param name="r">La instancia de random</param>
+        /// <returns>El ID si se genero con exito, -1 si no</returns>
+        private static int GenerateId(List<int> listOfProductsID, Random r)
+        {
+            int newID = r.Next(0, 10001);
+            bool exists;
+            try
+            {
+                List<Product> notebooksInBD = Factory.GetProductsFromDB("Notebooks");
+                List<Product> keyboardsInBD = Factory.GetProductsFromDB("Keyboards");
+                List<int> productsInBD_IDS = Factory.GetProductsSerialNumber(notebooksInBD);
+                productsInBD_IDS.AddRange(Factory.GetProductsSerialNumber(keyboardsInBD));
+                productsInBD_IDS.Sort();
+                listOfProductsID.Sort();
+                exists = Factory.ContainsID(productsInBD_IDS, ref newID);
+                exists = Factory.ContainsID(listOfProductsID, ref newID);
+                if (!(exists))
+                {
+                    listOfProductsID.Add(newID);
+                    return newID;
+                }
+            }
+            catch(SqlException ex)
+            {
+                throw ex;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return -1;
+        }
+        /// <summary>
+        /// Metodo que recorre lista de ID y compara contra uno generado con Random
+        /// Si se encuentra en la lista, le suma uno
+        /// </summary>
+        /// <param name="list">La lista de tipo int de IDs</param>
+        /// <param name="newID">El ID que se compara contra los IDs de la lista</param>
+        /// <returns>True si existe, False si no</returns>
+        private static bool ContainsID(List<int> list, ref int newID)
+        {
+            bool exists = true;
+            while (exists)
+            {
+                exists = list.Contains(newID);
+                if(exists)
+                    newID += 1;
+            }
+            return exists;
+        }
+        /// <summary>
+        /// Metodo que guarda en una lista de int los ID de cada Producto dentro de list
+        /// </summary>
+        /// <param name="list">La lista de Productos</param>
+        /// <returns>La lista de int (ids)</returns>
+        private static List<int> GetProductsSerialNumber(List<Product> list)
+        {
+            List<int> productsID = new List<int>();
+            foreach(Product item in list)
+            {
+                productsID.Add(item.Serial_Number);
+            }
+            return productsID;
+        }
+        /// <summary>
+        /// Metodo que obtiene de la BD los productos de acuerdo a la tabla pasada por param
+        /// </summary>
+        /// <param name="table">La tabla</param>
+        /// <returns>La lista de Productos</returns>
+        private static List<Product> GetProductsFromDB(string table)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=Products;Integrated Security=True";
+            List<Product> list;
+            try
+            {
+                list = SQL<Product>.QueryBD(connectionString, $"SELECT * from {table}");
+                return list;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Metodo de clase que incrementa el stock de materiales de acuerdo al tipo de producto que recibe por param
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Un objeto que tambien tiene un atributo materiales de tipo diccionario</param>
+        public static void LoadMoreStock<T>(T obj) where T : Product
+        {
+            Dictionary<string, int> temp = new Dictionary<string, int>(stock.StockList);
+            foreach (string key in temp.Keys)
+            {
+                foreach (string item in ((T)obj).MaterialsNeeded.Keys)
+                {
+                    if (key == item)
+                    {
+                        Thread.Sleep(13);
+                        if (obj is Keyboard)
+                        {
+                            stock.StockList[key] += random.Next(17, 53);
+                        }
+                        else
+                        {
+                            stock.StockList[key] += random.Next(3, 7);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Info Methodos
+        /// <summary>
+        /// Metodo que devuelve una lista de atributos de un objeto segun su Clase
+        /// </summary>
+        /// <typeparam name="T">La clase del objeto a obtener los atributos</typeparam>
+        /// <param name="list">La lista que contiene los objetos</param>
+        /// <param name="type">El nombre de la Clase</param>
+        /// <returns>La lista de atributos</returns>
+        public static List<string> GetProperties<T>(List<T> list, string type)
+        {
+            List<string> properties = new List<string>();
+            foreach (T item in list)
+            {
+                if (item.GetType().Name == type)
+                {
+                    foreach (var prop in item.GetType().GetProperties())
+                    {
+                        properties.Add(prop.Name);
+                    }
+                    break;
+                }
+            }
+            properties.Remove(properties.Last());
+            return properties;
+        }
+        /// <summary>
+        /// Metodo que recorre el diccionario de materiales (stock) y guarda informacion sobre el nombre y la cantidad,
+        /// en un string
+        /// </summary>
+        /// <returns>EL string creado a partir de todos los materiales</returns>
+        public static string StockInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("***** STOCK DISPONIBLE *****");
+            foreach (KeyValuePair<string, int> item in stock.StockList)
+            {
+                sb.AppendLine($"{item.Key} left: {item.Value}");
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Metodo que guarda informacion sobre c/producto de la lista de productos en un string
+        /// </summary>
+        /// <returns>El string creado a partir de los productos </returns>
+        public static string ProductsInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Product item in listaProductos)
+            {
+                if (item is Keyboard)
+                {
+                    sb.AppendLine("\n####### KEYBOARD #######");
+                }
+                else
+                {
+                    sb.AppendLine("\n####### NOTEBOOK #######");
+                }
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Recuperatorios/TP4/Lorenzo.Edgardo.2C.TPFinal/Test/Program.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesCore;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * 
+             *              PROBAMOS CREACION DE PRODUCTOS Y EXCEPCIONES
+             * 
+             * 
+             */
+
+            // ATENCION - LA MAYORIA DE LA PRUEBAS QUE RESULTAN SIN STOCK ES DEBIDO A LOS TECLADOS QUE CONSUMEN
+            // MUCHAS KEYCAPS Y MUCHOS SWTICHES, TENER EN CUENTA Y AGREGAR STOCK CONTINUO PARA PROBAR
+            bool sinStock = false;
+            // creamos 2 objetos. 
+            MechanicalKeyboard p1 = new MechanicalKeyboard("Poker3", 1500, EKeyboardSize.Tenkeyless, true, ESwitchColor.CherryBlue);
+            Thinkpad p2 = new Thinkpad("Thinkpad T420", 2000, EScreenSize.LargeScreen, 1, true);
+            // Cargamos el diccionario de materiales (de la clase Materials) con los nombres de los
+            // atributos de c/tipo de objeto y un valor random
+            Factory.stock.LoadMaterialsNeeded(p1);
+            Factory.stock.LoadMaterialsNeeded(p2);
+            // Aumentamos el stock que se necesita para crear c/producto
+            LoadStock(p1, p2);
+            // Mostramos Stock Inicial
+            CheckStock(1);
+            try
+            {
+                // Intentamos agregar 2 productos
+                // Cuando se testea agregar productos ESTAR atentos al LoadMoreStock y su random interno
+                // si es muy chico habra que llamarlo varias veces
+                // A modo de prueba se puede cambiar su random a uno mayor
+                Factory.Create = new Thinkpad("Thinkpad T420", 2500, EScreenSize.MediumScreen, 1, true);
+                Factory.Create = new MechanicalKeyboard("Poker3", 1500, EKeyboardSize.Tenkeyless, false, ESwitchColor.CherryBlue);
+                Console.WriteLine("Se agregan 2 productos (1)" + Factory.ProductsInfo());
+            }
+            catch (OutOfStockException ex)
+            {
+                sinStock = true;
+                Console.WriteLine($"No hay stock suficiente (1) ", ex.Message);
+
+            }
+            try
+            {
+                Factory.Create = new MechanicalKeyboard("Qisan Magicforce", 1150, EKeyboardSize.Small, true, ESwitchColor.GateronClear);
+                Factory.Create = new MechanicalKeyboard("Das Keyboard 100", 1790, EKeyboardSize.Small, false, ESwitchColor.GateronBrown);
+                Console.WriteLine("Se agregan 2 productos (2)" + Factory.ProductsInfo());
+            }
+            catch (OutOfStockException ex)
+            {
+                sinStock = true;
+                Console.WriteLine("No hay stock suficiente (2) ", ex.Message);
+            }
+
+            if (sinStock)
+            {
+                LoadStock(p1, p2);
+                sinStock = false;
+            }
+            CheckStock(2);
+            try
+            {
+                Factory.Create = new MechanicalKeyboard("Das Keyboard 100", 2655, EKeyboardSize.FullSize, false, ESwitchColor.CherryRed);
+                Factory.Create = new Thinkpad("Thinkpad T430", 2500, EScreenSize.LargeScreen, 1, true);
+                Console.WriteLine("Se agregan 2 productos (3)" + Factory.ProductsInfo());
+            }
+            catch (OutOfStockException ex)
+            {
+                Console.WriteLine("Te quedaste sin stock (3) ", ex.Message);
+
+            }
+            CheckStock(3);
+
+            /*
+             * 
+             * 
+             *              PROBAMOS SERIALIZACION
+             * 
+             * 
+             */
+            Console.WriteLine("\n@@@@@@ SE PRUEBA SERIALIZACION @@@@@\n");
+            try
+            {
+                // Corroboramos que la lista de productos sea mayor a 0
+                // Si no lo es, lanzamos excepcion
+                DateTime currentDate = new DateTime();
+                currentDate = DateTime.Now;
+                if (!(Factory.listaProductos.Count > 0))
+                {
+                    throw new NoProductCreatedException("Stock is empty, nothing to be serialized\nGo build some");
+                }
+                string path = AppDomain.CurrentDomain.BaseDirectory + "ProductsList - " + currentDate.ToString("-MMddyyyy_HHmmss") + ".xml";
+                Serializator<List<Product>> toXml = new Serializator<List<Product>>();
+                toXml.SaveXML(path, Factory.listaProductos);
+                Console.WriteLine($"XML file created successfully at {AppDomain.CurrentDomain.BaseDirectory}");
+            }
+            catch (NoProductCreatedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (sinStock)
+            {
+                LoadStock(p1, p2);
+                sinStock = false;
+            }
+            CheckStock(4);
+            try
+            {
+                Factory.Create = new MechanicalKeyboard("Das Keyboard 100", 2655, EKeyboardSize.FullSize, false, ESwitchColor.CherryRed);
+                Factory.Create = new Thinkpad("Thinkpad T430", 2500, EScreenSize.LargeScreen, 1, true);
+                Factory.Create = new Thinkpad("Thinkpad T450", 3500, EScreenSize.LargeScreen, 1, true);
+                Console.WriteLine("Se agregan 2 productos (4)" + Factory.ProductsInfo());
+            }
+            catch (OutOfStockException ex)
+            {
+                sinStock = true;
+                Console.WriteLine("Te quedaste sin stock (4) ", ex.Message);
+
+            }
+            CheckStock(5);
+
+            /*
+             * 
+             * 
+             *              PROBAMOS GUARDAR EN DB
+             * 
+             * 
+             */
+            Console.WriteLine("\n@@@@@@ SE PRUEBA BASE DATOS @@@@@\n");
+            string notebookQuery = null;
+            string keyboardQuery = null;
+            List<string> thinkpadProperties = null;
+            List<string> keyboardProperties = null;
+            string connectionString = "Data Source=.;Initial Catalog=Products;Integrated Security=True";
+            if (Factory.listaProductos.Count > 0)
+            {
+                if (Factory.listaProductos.ContainsType("Thinkpad")) // metodo de extension
+                {
+                    thinkpadProperties = Factory.GetProperties(Factory.listaProductos, "Thinkpad");
+                    notebookQuery = SQL<Thinkpad>.BuildInsertQuery(thinkpadProperties, "Notebooks");
+                }
+                if (Factory.listaProductos.ContainsType("MechanicalKeyboard")) // metodo de extension
+                {
+                    keyboardProperties = Factory.GetProperties(Factory.listaProductos, "MechanicalKeyboard");
+                    keyboardQuery = SQL<MechanicalKeyboard>.BuildInsertQuery(keyboardProperties, "Keyboards");
+                }
+                try
+                {
+                    List<Product> temp = new List<Product>(Factory.listaProductos);
+                    foreach (Product item in temp)
+                    {
+                        if (item is Thinkpad)
+                        {
+                            SQL<Thinkpad>.Insert(connectionString, notebookQuery, thinkpadProperties, (Thinkpad)item);
+                        }
+                        if (item is MechanicalKeyboard)
+                        {
+                            SQL<MechanicalKeyboard>.Insert(connectionString, keyboardQuery, keyboardProperties, (MechanicalKeyboard)item);
+                        }
+                        Factory.listaProductos.Remove(item);
+                    }
+                    Console.WriteLine("Product/s successfully save to DB. Stock emptied");
+                }
+                catch (InvalidQueryException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stock is empty, nothing to save on DB\nGo build some");
+            }
+
+            /*
+             * 
+             * 
+             *              PROBAMOS CONSULTAS BD
+             * 
+             * 
+             */
+
+            Console.WriteLine("\n@@@@@@ SE PRUEBA CONSULTAS A DATOS @@@@@\n");
+            List<Product> listN = null;
+            List<Product> listK = null;
+
+            try
+            {
+                listN = SQL<Product>.QueryBD(connectionString, "SELECT * from Notebooks");
+                listK = SQL<Product>.QueryBD(connectionString, "SELECT * from Keyboards");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("\n@@@@@@ TABLA: NOTEBOOKS @@@@@\n");
+                if (listN != null && listN.Count > 0)
+                {
+                    foreach (Product item in listN)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                }
+
+                Console.WriteLine("\n@@@@@@ TABLA: TECLADOS @@@@@\n");
+                if (listK != null && listK.Count > 0)
+                {
+                    foreach (Product item in listK)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("OJO, LISTA NULA. CHEQUEAR" + ex.Message);
+            }
+
+
+            Console.WriteLine("FINAL DE LA CLASE TEST");
+            Console.ReadKey();
+        }
+
+
+        /*
+         * 
+         * METODOS STATICOS DE AYUDA
+         * 
+         */
+
+        public static void LoadStock(MechanicalKeyboard m, Thinkpad t)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Factory.LoadMoreStock(t);
+                Factory.LoadMoreStock(m);
+            }
+        }
+
+        private static void CheckStock(int n)
+        {
+            Console.WriteLine("\n##################################");
+            Console.WriteLine("##################################");
+            Console.WriteLine($"@@@@ CONSULTA DE STOCK N: {n} @@@@");
+            Console.WriteLine(Factory.StockInfo());
+            Console.WriteLine("##################################");
+            Console.WriteLine("##################################\n");
+        }
+    }
+}
